b0e93c411c6afd4b4b26053be7963ff7365dbb31
Ziad Attia <65275092+Ziad-Attia@users.noreply.github.com>
2021-09-19 15:55:47 -0400

    use os.path

--- geo.py
+++ geo.py
@@ -1,10 +1,13 @@
 from numpy import NAN
 import pandas as pd
 import re
+import os
+import os.path
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
 
-data = pd.read_excel('D:/bird data/Bird_Ranges_AGJ edits.xlsx')
+path = os.path.dirname(os.path.realpath(__file__)) + '/'
+data = pd.read_excel(path + 'Bird_Ranges_AGJ edits.xlsx')
 addressesNorth  = pd.DataFrame(columns = ["Bird", "City1", "City2", "City3", "City4", "City5", "City6"])
 addressesSouth  = pd.DataFrame(columns = ["Bird", "City1", "City2", "City3", "City4", "City5", "City6"])
 
@@ -65,7 +68,7 @@ for i in range(1,7):
     geoNorth['City'+str(i)+"-Longitude"] = addressesNorth['location'+str(i)].apply(lambda loc: loc.point.longitude if loc else None)
     geoSouth['City'+str(i)+"-Latitude"] = addressesSouth['location'+str(i)].apply(lambda loc: loc.point.latitude if loc else None)
     geoSouth['City'+str(i)+"-Longitude"] = addressesSouth['location'+str(i)].apply(lambda loc: loc.point.longitude if loc else None)
-geoNorth.to_csv('D:/bird data/geoNorth.csv')
-geoSouth.to_csv('D:/bird data/geoSouth.csv')
-addressesNorth.to_csv('D:/bird data/addressesNorth.csv')
-addressesSouth.to_csv('D:/bird data/addressesSouth.csv')
+geoNorth.to_csv(path + 'geoNorth.csv')
+geoSouth.to_csv(path + 'geoSouth.csv')
+addressesNorth.to_csv(path + 'addressesNorth.csv')
+addressesSouth.to_csv(path + 'addressesSouth.csv')
